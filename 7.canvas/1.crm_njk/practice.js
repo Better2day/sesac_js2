03d04fe8b45575eef414fd924a7ada66014ce69e
Joon <wushu80@hanmail.net>
2024-11-26 21:54:16 +0900

    11.26 학습

--- 7.canvas/1.crm_njk/practice.js
+++ 7.canvas/1.crm_njk/practice.js
@@ -43,4 +43,22 @@ const copyPerson = person;
 copyPerson.name = '이도';
 person['age'] = 10;
 console.log(person);
- 
+
+const numbers = Array(3).fill(10).map((v, i) => v + i);
+console.log(numbers);
+
+
+// Array functions
+// 값만 사용할 때는 인수로 i를 받지 않아도 됨
+// 그러나 인덱스를 사용하고 싶으면, 값을 사용하지 않아도 둘 다 인수로 받아야 함
+const array = [1, 3, 5, 7];
+const array2 = array.filter((v) => {
+    return v > 1;
+});
+console.log(array2);
+
+// ※ reduce 함수는 초기값을 주지 않으면 자동으로 1로 설정하고, 배열의 2번째 요소부터 연산을 시작한다.
+// 초기값을 줬을 때와 주지 않았을 때 동작이 다르니, 사용할 때 주의해야 한다!
+const array3 = [1, 2, 3, 4, 5].reduce((a, c) => { console.log(a, c);
+                                                  return a + c });
+                                               // return a + c }, 0);
