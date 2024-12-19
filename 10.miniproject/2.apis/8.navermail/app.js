ffcf77f324f1fc229554d8585954fc6898628769
Joon <wushu80@hanmail.net>
2024-12-19 22:41:31 +0900

    mail API 복습

--- 10.miniproject/2.apis/8.navermail/app.js
+++ 10.miniproject/2.apis/8.navermail/app.js
@@ -72,9 +72,54 @@ app.post('/signup', (req, res) => {
 
 app.post('/verify', (req, res) => {
     const { email, code } = req.body;
-    console.log('입력값: ', email, code);
-    console.log('우리 DB: ', database);
+    console.log(`FE 입력값 - email: ${email}, code: ${code}`);
+    // console.log('우리 DB: ', database);
     // To-do 여기에서 두 개를 비교해서 사용자에게 응답 주기
+    /* 
+        if (database.users.filter((user) => (user.email === email && user.code === code))) {
+            res.json({ message: '인증되었습니다!' });
+        } else {
+            res.status(404).json({ message: '인증에 실패했습니다.' });
+        }
+     */
+
+    // console.log('database.users: ', database.users);
+
+    // const user = database.users.filter((user) => user.email === email);
+    // filter는 필터링된 결과가 한 개여도 배열로 반환 ∴ 다시 forEach 등 순환문을 돌리면서 확인해야 함
+    // 이 결과값으로 순환문 없이 그냥 비교하려고 했으니 undefined
+
+    const user = database.users.find((user) => user.email === email);
+    // find는 조건에 맞는 것 한 개만 찾으면 바로 요소(배열 아님)를 반환하고 종료
+    console.log('user: ', user);
+
+    // console.log(`BE 임시 사용자 데이터: email: ${user.email}, code: ${user.code}`);
+
+    if (user.email === email) {
+        if (user.code === code) {
+            res.json({ message: '인증되었습니다!' });
+        } else {
+            res.json({ message: '인증 코드가 틀렸습니다.' });
+        }
+    } else {
+        res.status(401).json({ message: '인증에 실패했습니다.' });
+    };
+
+    // 제일 쉽게, forEach 순환문 돌리면서 비교
+    /* 
+        database.users.forEach(user => {
+            console.log(`BE 임시 사용자 데이터: email: ${user.email}, code: ${user.code}`);
+            if (user.email === email) {
+                if (user.code === code) {
+                    res.json({ message: '인증되었습니다!' });
+                } else {
+                    res.json({ message: '인증 코드가 틀렸습니다.' });
+                }
+            } else {
+                res.status(401).json({ message: '인증에 실패했습니다.' });
+            };
+        });
+    */
 });
 
 app.listen(PORT, () => {
--- 10.miniproject/2.apis/8.navermail/naversender.js
+++ 10.miniproject/2.apis/8.navermail/naversender.js
@@ -1,19 +1,24 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+console.log(process.env.NAVER_EMAIL);
+console.log(process.env.NAVER_PASSWORD);
+
 const transporter = nodemailer.createTransport({
     host: 'smtp.naver.com',
-    port: 465,
+    port: 587, // pop/smtp를 이용할 때는 465, imap/smtp는 587
     auth: {
         user: process.env.NAVER_EMAIL,
         pass: process.env.NAVER_PASSWORD,
-    }
+    },
+    logger: true,
+    debugger: true,
 })
 
 const mailOptions = {
     from: process.env.NAVER_EMAIL,
     to: process.env.SENDTO_EMAIL,
-    subject: '테스트 이메일',
+    subject: '테스트 이메일 3 (POP 사용 안 함 상태로 전송)',
     text: '안녕하세요, 메일 전송 API 테스트중입니다! 회원 가입을 완료하기 위해서는 다음 코드를 입력하세요 (136254)',
 }
 
